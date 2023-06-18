14598f86b0e848cc08dc06cdb7c8c0ce1c8caecb
n.asadipour <n.asadipour@charisma.ir>
2023-06-18 15:26:33 +0330

    Add project files.

---
+++ DddInPractice.Logic/Money.cs
@@ -0,0 +1,41 @@
+﻿namespace DddInPractice.Logic
+{
+    public class Money
+    {
+        public int OneCentCount { get; private set; }
+        public int TenCentCount { get; private set; }
+        public int QuarterCount { get; private set; }
+        public int OneDollarCount { get; private set; }
+        public int FiveDollarCount { get; private set; }
+        public int TwentyDollarCount { get; private set; }
+
+        public Money(
+            int oneCentCount,
+            int tenCentCount,
+            int quarterCount,
+            int oneDollarCount,
+            int fiveDollarCount,
+            int twentyDollarCount)
+        {
+            OneCentCount = oneCentCount;
+            TenCentCount = tenCentCount;
+            QuarterCount = quarterCount;
+            OneDollarCount = oneDollarCount;
+            FiveDollarCount = fiveDollarCount;
+            TwentyDollarCount = twentyDollarCount;
+        }
+
+        public static Money operator +(Money money1, Money money2)
+        {
+            Money sum = new Money(
+                money1.OneCentCount + money2.OneCentCount,
+                money1.TenCentCount + money2.TenCentCount,
+                money1.QuarterCount + money2.QuarterCount,
+                money1.OneDollarCount + money2.OneDollarCount,
+                money1.FiveDollarCount + money2.FiveDollarCount,
+                money1.TwentyDollarCount + money2.TwentyDollarCount);
+
+            return sum;
+        }
+    }
+}
---
+++ DddInPractice.Logic/SnackMachine.cs
@@ -0,0 +1,25 @@
+﻿namespace DddInPractice.Logic
+{
+    public sealed class SnackMachine : Entity
+    {
+        public Money MoneyInside { get; private set; }
+        public Money MoneyInTransaction { get; private set; }
+
+        public void InsertMoney(Money money)
+        {
+            MoneyInTransaction += money;
+        }
+
+        public void ReturnMoney()
+        {
+            //MoneyInTransaction = 0;
+        }
+
+        public void BuySnack()
+        {
+            MoneyInside += MoneyInTransaction;
+
+            //MoneyInTransaction = 0;
+        }
+    }
+}
